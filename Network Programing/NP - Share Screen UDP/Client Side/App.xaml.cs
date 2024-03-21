abf45b249e13ccb590000c037a9b5994f0639048
Orkhan <orkhanm07@gmail.com>
2024-03-21 17:19:51 +0400

    StepIT

---
+++ Network Programing/NP - Share Screen UDP/Client Side/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Client_Side;
+
+public partial class App : Application
+{
+}
---
+++ Network Programing/NP - Share Screen UDP/Server Side/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listenerEP = new IPEndPoint(ip, port);
+var listener = new UdpClient(listenerEP);
+Console.WriteLine($"{listenerEP} Listening...");
+
+while (true)
+{
+    var result = await listener.ReceiveAsync();
+    Console.WriteLine($"{result.RemoteEndPoint} Connected...");
+    _ = Task.Run(async () =>
+    {
+        IPEndPoint? remoteEP = result.RemoteEndPoint;
+
+
+        while (true)
+        {
+            var screenImage = await TakeScreenShotAsync();
+            var imgBytes = await ImageToByteAsync(screenImage);
+
+            var chunks = imgBytes?.Chunk(ushort.MaxValue - 29);
+
+            foreach (var chunk in chunks!)
+                await listener.SendAsync(chunk, chunk.Length, remoteEP);
+        }
+    });
+}
+
+async Task<Image?> TakeScreenShotAsync()
+{
+
+    var width = Screen.PrimaryScreen.Bounds.Width;
+    var height = Screen.PrimaryScreen.Bounds.Height;
+
+    Bitmap? bitmap = new Bitmap(width, height);
+
+    using Graphics graphics = Graphics.FromImage(bitmap);
+    graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+    return bitmap;
+}
+
+async Task<byte[]?> ImageToByteAsync(Image? image)
+{
+    using MemoryStream ms = new MemoryStream();
+    image?.Save(ms, ImageFormat.Jpeg);
+
+    return ms.ToArray();
+}
