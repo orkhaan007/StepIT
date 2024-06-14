15845ea9feadf0b656a57fe3f5bd74af22a598dd
Orkhan <orkhanm07@gmail.com>
2024-06-14 13:15:34 +0400

    StepIT

---
+++ React/JSX -Review/src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Statistics from './components/Statistics';
+import FeedbackOptions from './components/FeedbackOptions';
+import Section from './components/Section';
+import Notification from './components/Notification';
+
+function App() {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (type) => {
+    setState((prevState) => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    return state.good + state.neutral + state.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total === 0 ? 0 : Math.round((state.good / total) * 100);
+  };
+
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <div>
+      <h1>Expresso Cafe Feedback</h1>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {totalFeedback === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedbackPercentage}
+          />
+        )}
+      </Section>
+    </div>
+  );
+}
+
+export default App;
