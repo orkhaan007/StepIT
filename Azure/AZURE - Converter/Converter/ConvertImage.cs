8e71bf7d8c247444684fd7d11e869f720611cb11
orkhaan007 <orkhanm07@gmail.com>
2024-12-04 22:08:39 +0400

    StepIT

---
+++ Azure/AZURE - Converter/Converter/ConvertImage.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Converter
+{
+    public static class ConvertImage
+    {
+        [FunctionName("ConvertImage")]
+        public static void Run(
+            [BlobTrigger("images-original/{name}", Connection = "AzureWebJobsStorage")] Stream inputBlob,
+            string name,
+            [Blob("images/{name}.jpeg", FileAccess.Write, Connection = "AzureWebJobsStorage")] Stream outputBlob,
+            ILogger log)
+        {
+            log.LogInformation($"Processing blob: {name}");
+
+            try
+            {
+                using (var image = Image.FromStream(inputBlob))
+                {
+                    image.Save(outputBlob, ImageFormat.Jpeg);
+                }
+
+                log.LogInformation($"Converted {name} to JPEG.");
+            }
+            catch (System.Exception ex)
+            {
+                log.LogError($"Error converting image {name}: {ex.Message}");
+            }
+        }
+    }
+}
